8eba33ba079105bfadc6c790b01b9f83ff8c0986
Mafia0331 <J.K.play2015@gmail.com>
2024-03-11 14:37:33 +0200

    create user service

---
+++ src/main/java/org/spring/todo/todolist/services/UserService.java
@@ -0,0 +1,43 @@
+package org.spring.todo.todolist.services;
+
+import lombok.RequiredArgsConstructor;
+import org.spring.todo.todolist.models.User;
+import org.spring.todo.todolist.repo.RoleRepository;
+import org.spring.todo.todolist.repo.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(String.format("Пользователь %s не найден", username)));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList()));
+    }
+
+    public void createNewUser (User user) {
+        user.setRoles(List.of(roleRepository.findByName("ROLE_USER").get()));
+        userRepository.save(user);
+    }
+}
