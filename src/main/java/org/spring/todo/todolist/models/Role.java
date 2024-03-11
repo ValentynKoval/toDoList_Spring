d3491fd5dce57277f446dd2b6b301a7e0a1a734f
Mafia0331 <J.K.play2015@gmail.com>
2024-03-11 14:25:35 +0200

    create roles model

--- src/main/java/org/spring/todo/todolist/models/Role.java
+++ src/main/java/org/spring/todo/todolist/models/Role.java
@@ -5,12 +5,10 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "users")
-public class User {
+@Document(collection = "roles")
+public class Role {
     @Id
     private String id;
 
-    private String username;
-    private String password;
-    private String email;
+    private String name;
 }
