12b9b54068188c676325dd58a6120509a8db7a7c
Mafia0331 <J.K.play2015@gmail.com>
2024-03-12 17:03:15 +0200

    new method for creating new role

--- src/main/java/org/spring/todo/todolist/services/RoleService.java
+++ src/main/java/org/spring/todo/todolist/services/RoleService.java
@@ -1,6 +1,7 @@
 package org.spring.todo.todolist.services;
 
 import lombok.RequiredArgsConstructor;
+import org.spring.todo.todolist.dto.RoleDto;
 import org.spring.todo.todolist.models.Role;
 import org.spring.todo.todolist.repo.RoleRepository;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,11 @@ public class RoleService {
 
     public Role getUserRole() {
         return roleRepository.findByName("ROLE_USER").get();
+    }
+
+    public Role createNewRole(RoleDto roleDto) {
+        Role role = new Role();
+        role.setName(roleDto.getName());
+        return roleRepository.save(role);
     }
 }
