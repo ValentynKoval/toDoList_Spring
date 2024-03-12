58b7edc7162f4c478cc949a4ea698713cb633f78
Mafia0331 <J.K.play2015@gmail.com>
2024-03-12 16:35:48 +0200

    create new endpoint

--- src/main/java/org/spring/todo/todolist/controllers/AuthController.java
+++ src/main/java/org/spring/todo/todolist/controllers/AuthController.java
@@ -1,12 +1,12 @@
 package org.spring.todo.todolist.controllers;
 
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.spring.todo.todolist.dto.AuthDto;
-import org.spring.todo.todolist.dto.TokenResponseDto;
+import org.spring.todo.todolist.dto.RegistrationUserDto;
 import org.spring.todo.todolist.exceptions.AppError;
+import org.spring.todo.todolist.models.User;
 import org.spring.todo.todolist.services.JwtService;
 import org.spring.todo.todolist.services.UserService;
 import org.springframework.http.HttpStatus;
@@ -33,10 +33,26 @@ public class AuthController {
         } catch (BadCredentialsException e) {
             return new ResponseEntity<>(new AppError(HttpStatus.UNAUTHORIZED.value(), "Неправильный логин или пароль"), HttpStatus.UNAUTHORIZED);
         }
+        return loginUser(response, authDto);
+    }
+
+    public ResponseEntity loginUser(HttpServletResponse response, AuthDto authDto) {
         UserDetails userDetails = userService.loadUserByUsername(authDto.getUsername());
         String token = jwtService.generateToken(userDetails);
         Cookie cookie = new Cookie("token", token);
         response.addCookie(cookie);
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<?> createNewUser(HttpServletResponse response, @RequestBody RegistrationUserDto registrationUserDto) {
+        if (!registrationUserDto.getPassword().equals(registrationUserDto.getConfirmPassword())) {
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "Пароли не совпадают"), HttpStatus.BAD_REQUEST);
+        }
+        if (userService.findByUsername(registrationUserDto.getUsername()) != null) {
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "Пользователь с таким именем уже существует"), HttpStatus.BAD_REQUEST);
+        }
+        User user = userService.createNewUser(registrationUserDto);
+        return loginUser(response, new AuthDto(user.getUsername(), user.getPassword()));
     }
 }
