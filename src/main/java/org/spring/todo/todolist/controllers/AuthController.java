d7aeb65b3907daa3784c06d7af4f371765868382
Mafia0331 <J.K.play2015@gmail.com>
2024-03-13 18:13:42 +0200

    add a new controller for refresh tokens

--- src/main/java/org/spring/todo/todolist/controllers/AuthController.java
+++ src/main/java/org/spring/todo/todolist/controllers/AuthController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.spring.todo.todolist.dto.AuthDto;
 import org.spring.todo.todolist.dto.RegistrationUserDto;
+import org.spring.todo.todolist.dto.UsernameDto;
 import org.spring.todo.todolist.services.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +19,17 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> userAuthorization(HttpServletResponse response, @RequestBody AuthDto authDto) {
+
         return authService.createAuthTokens(response, authDto);
     }
-
 
     @PostMapping("/registration")
     public ResponseEntity<?> createNewUser(HttpServletResponse response, @RequestBody RegistrationUserDto registrationUserDto) {
         return authService.createNewUser(response, registrationUserDto);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refreshTokens(HttpServletResponse response, @RequestBody UsernameDto usernameDto) {
+        return authService.refreshTokens(response, usernameDto.getUsername());
     }
 }
